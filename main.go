86884e147ce9417dcdf2e27a4d1d30c39cd183f9
EricSchrock <eric.schrock22@gmail.com>
2026-07-28 23:51:57 +0000

    Use a channel

--- main.go
+++ main.go
@@ -13,30 +13,48 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	buff := make([]byte, 8)
-	line := ""
-	for {
-		n, err := f.Read(buff)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		} else if err != io.EOF && n <= 0 {
-			log.Fatal("No bytes read")
-		} else if n <= 0 {
-			break
-		}
+	ch := getLinesChannel(f)
 
-		parts := bytes.Split(buff[:n], []byte{'\n'})
-		line += string(parts[0])
-		for i := 1; i < len(parts); i++ {
-			fmt.Printf("read: %s\n", line)
-			line = string(parts[i])
-		}
-
-		if err == io.EOF {
-			break
-		}
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
 	}
-	fmt.Printf("read: %s\n", line)
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+
+		buff := make([]byte, 8)
+		line := ""
+		for {
+			n, err := f.Read(buff)
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			} else if err != io.EOF && n <= 0 {
+				log.Fatal("No bytes read")
+			} else if n <= 0 {
+				break
+			}
+
+			parts := bytes.Split(buff[:n], []byte{'\n'})
+			line += string(parts[0])
+			for i := 1; i < len(parts); i++ {
+				ch <- line
+				line = string(parts[i])
+			}
+
+			if err == io.EOF {
+				break
+			}
+		}
+		if line != "" {
+			ch <- line
+		}
+	}()
+
+	return ch
 }
