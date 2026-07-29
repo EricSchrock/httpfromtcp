452851b5d9b65eaf4e299348aae2fe40a2c19c74
EricSchrock <eric.schrock22@gmail.com>
2026-07-29 00:59:00 +0000

    UDP client

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:42069")
+	if err != nil {
+		log.Fatalf("Failed to resolve UDP address: %v", err.Error())
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("Failed to dial UDP address: %v", err.Error())
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("Failed to read input: %v", err.Error())
+		}
+
+		_, err = conn.Write([]byte(text))
+		if err != nil {
+			log.Printf("Failed to write to connection: %v", err.Error())
+		}
+	}
+}
