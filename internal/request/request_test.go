4ebbc03f4a5822da94e84981671a10bdb3972837
EricSchrock <eric.schrock22@gmail.com>
2026-08-11 23:09:58 +0000

    Split unit tests

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -8,64 +8,74 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRequestLineParse(t *testing.T) {
-	// Test: Good GET Request line
+func TestParseGetRequestLine(t *testing.T) {
 	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
 
-	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseGetRequestLineWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
 
-	// Test: Good POST Request line with path
-	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParsePostRequestLineWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "POST", r.RequestLine.Method)
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
 
-	// Test: Invalid number of parts in request line (too few)
-	r, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithTooFewParts(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid number of parts in request line (too many)
-	r, err = RequestFromReader(strings.NewReader("GET /cof fee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithTooManyParts(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET /cof fee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid request method
-	r, err = RequestFromReader(strings.NewReader("GOT /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithInvalidMethod(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GOT /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid request method (capitalization)
-	r, err = RequestFromReader(strings.NewReader("Get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithUncapitalizedMethod(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("Get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid request method (out of order)
-	r, err = RequestFromReader(strings.NewReader("HTTP/1.1 Get /coffee\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineOutOfOrder(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("HTTP/1.1 Get /coffee\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid number of parts in HTTP version (too few)
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithTooFewVersionParts(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET /coffee HTTP1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid number of parts in HTTP version (too many)
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithTooManyVersionParts(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid HTTP version (first part)
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTPS/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithInvalidFirstVersionPart(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET /coffee HTTPS/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+}
 
-	// Test: Invalid HTTP version (second part)
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+func TestParseRequestLineWithInvalidSecondVersionPart(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
 }
